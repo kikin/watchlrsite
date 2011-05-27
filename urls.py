d1faede03ef90081ea095547626d9fe087bf8b1f
Nick Zaillian <naz2106@columbia.edu>
2011-05-27 12:39:20 -0400

    added a static files directory, modified settings accordingly

--- urls.py
+++ urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.static import static
+import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -16,3 +18,5 @@ urlpatterns = patterns('',
     # url(r'^admin/', include(admin.site.urls)),
 	(r'^$', include('kikinvideo.app.urls')),
 )
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
