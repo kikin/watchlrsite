22329167addae63c3bb29865f09d9a857600e802
Nick Zaillian <naz2106@columbia.edu>
2011-05-27 14:48:28 -0400

    more work on templatizing our existing site

--- app/views.py
+++ app/views.py
@@ -5,8 +5,13 @@ from kikinvideo import settings
 #note: once User class has been defined, we will almost certainly be passing
 #that through to each template in the context hash when we render...
 
+#example user context:
+ex_user = {'username':'JenBear', 'realName':'Jennifer Lee', 'followingCount':200,\
+           'followerCount':300, 'videosCount':300, 'imageURL' : '/static/images/profile/ex_profile_pic.png'}
+
 def home(request):
 	return render_to_response('user_home.html', {'settings':settings})
 
+#hard coding tag bindings so you can see how this will work...
 def profile(request):
-	return render_to_response('profile.html', {'settings':settings})
+	return render_to_response('profile.html', {'settings':settings, 'user': ex_user })
