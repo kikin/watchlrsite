dc07444a0b491c50aab03448328fcd5cbcb35984
Nick Zaillian <naz2106@columbia.edu>
2011-05-27 14:19:50 -0400

    added template/controller (view) for profile

--- app/views.py
+++ app/views.py
@@ -2,5 +2,11 @@
 from django.shortcuts import render_to_response
 from kikinvideo import settings
 
+#note: once User class has been defined, we will almost certainly be passing
+#that through to each template in the context hash when we render...
+
 def home(request):
 	return render_to_response('user_home.html', {'settings':settings})
+
+def profile(request):
+	return render_to_response('profile.html', {'settings':settings})
