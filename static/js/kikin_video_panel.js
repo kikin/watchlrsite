ad28a97de857766ccfe10ec29d5ae04be2c0dd17
Nick Zaillian <naz2106@columbia.edu>
2011-05-28 11:55:05 -0400

    about to namespace the existing (non-third party) scripts

--- static/js/kikin_video_panel.js
+++ static/js/kikin_video_panel.js
@@ -1,5 +1,7 @@
-Cufon.replace('h3.video-title, .section-title, h4', {
-    fontFamily: 'vag',
-    forceHitArea: true,
-    hover: true
+$(document).ready(function(){
+    Cufon.replace('h3.video-title, .section-title, h4', {
+        fontFamily: 'vag',
+        forceHitArea: true,
+        hover: true
+    });
 });
--- webapp/views.py
+++ webapp/views.py
@@ -5,7 +5,7 @@ from kikinvideo import settings
 #note: once User class has been defined, we will almost certainly be passing
 #that through to each template in the context hash when we render...
 
-ex_video = {'title':'Can I Kick It (video)', 'description':'Great video, great loop ...',\
+ex_video = {'title':'Can I Kick It (video)', 'description':"See Music Videos http://www.bvmtv.com/ that you CAN'T See on You Tube! even some X RATED music videos! +Live Chat and Embed video codes.   Pete Rock & C.L. Smooth were an influential rap group from Mount Vernon, New York. They made their debut in the rap world with their 1991 EP, All Souled Out. It sold moderately well enough to justify Elektra Records clearing",\
             'url':'http://www.youtube.com/watch?v=UbDFS6cg1AI', 'thumbnail': 'http://i.ytimg.com/vi/UbDFS6cg1AI/0.jpg',\
             'embed_code':'http://www.youtube.com/v/UbDFS6cg1AI?version=3&autoplay=1', 'id':22, 'likes':1, 'liked':1}
 #example user context:
