6b35647a29c1858e36f27097c77e10a612aecbe8
Nick Zaillian <naz2106@columbia.edu>
2011-05-28 00:56:35 -0400

    added url-routing logic for api package, plus one view stub

---
+++ api/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls.defaults import patterns, include, url
+from kikinvideo import settings
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('api',
+    (r'^auth/profile', 'views.authenticate'),
+    (r'^list', 'views.list'),
+    (r'^save', 'views.save'),
+    (r'remove', 'views.remove'),
+    (r'update', 'views.update'),
+)
--- urls.py
+++ urls.py
@@ -17,6 +17,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 	(r'^', include('kikinvideo.webapp.urls')),
+    (r'^api/', include('kikinvideo.api.urls'))
 )
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
