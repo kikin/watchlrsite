b71f8cc59e87b918f1cc6e65bbf80f51df7964a4
Nick Zaillian <naz2106@columbia.edu>
2011-05-28 16:07:08 -0400

    Touched up models because certain things became apparent when writing scripts/test/db_populate.py (script to populate db with fake data for testing).  i.e. user->video relation table fields needed sensible defaults (makes sense for UserLikesVideo relation's 'watched' property to default to True, for instance..., though when creating instances you can set the property to false if you want...)

--- scripts/test/db_populate.py
+++ scripts/test/db_populate.py
@@ -1,4 +1,4 @@
-import sys, os
+import sys, os, datetime
 
 # the settings module is in the project root package 3 dirs up...
 # (ASSUMING you are execting this script from within the 
@@ -22,7 +22,24 @@ v_source_1.favicon_url = 'http://s.ytimg.com/yt/favicon-vflZlzSbU.ico'
 v_source_1.save()
 
 v_1 = Video()
+v_1.url = "http://www.youtube.com/watch?v=UbDFS6cg1AI"
+v_1.title='Can I Kick It?'
+v_1.description = "See Music Videos http://www.bvmtv.com/ that you CAN'T See on You Tube! even some X RATED music videos! +Live Chat and Embed video codes.   Pete Rock & C.L. Smooth were an influential rap group from Mount Vernon, New York. They made their debut in the rap world with their 1991 EP, All Souled Out. It sold moderately well enough to justify Elektra Records clearing"
+v_1.thumbnail = v_thumb_1
+v_1.source = v_source_1
+v_1.last_updated = datetime.datetime.now()
+v_1.save()
 
-nick = User()
-nick.name = "Nicholas Zaillian"
+user_1 = User()
+user_1.name = "Steve Williams"
+user_1.email = "w@jkblkjkb.com"
+user_1.save()
 
+user_1_likes_v1 = UserLikedVideo(user=user_1, video=v_1)
+user_1_likes_v1.save()
+
+user_1_saved_v1 = UserSavedVideo(user=user_1, video=v_1)
+user_1_saved_v1.save()
+
+user_1_watched_v1 = UserWatchedVideo(user=user_1, video=v_1)
+user_1_watched_v1.save()
