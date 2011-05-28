99876f33c0ef5f364d5d3417320eb4a119ebf058
Nick Zaillian <naz2106@columbia.edu>
2011-05-27 22:50:54 -0400

    Models defined, including relation models (with necessary additional properties/columns) for the user.saved_videos, user.liked_videos, user.watched_videos.  Have tested on my own machine with MySQL server.

--- models.py
+++ models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+class User(models.Model):
+	id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=200)
+	email = models.CharField(max_length=500)
+	facebook_access_token = models.CharField(max_length=100)
+	saved_videos = models.ManyToManyField('Video', related_name='saved_videos', through='UserSavedVideo')
+	liked_videos = models.ManyToManyField('Video', related_name='liked_videos', through='UserLikedVideo')
+	watched_videos = models.ManyToManyField('Video', related_name='watched_videos', through='UserWatchedVideo')
+	followed_users = models.ManyToManyField('User')
+
 class Video(models.Model):
 	id = models.AutoField(primary_key=True)
 	url = models.CharField(max_length=1000)
@@ -16,3 +26,25 @@ class VideoSource(models.Model):
 	domain = models.CharField(max_length=1000)
 	favicon_url = models.CharField(max_length=1000)
 	videos = models.ManyToManyField('Video')
+
+#associate necessary additional info with user-video mappings...
+
+#These models define the properties of the relation tables
+#mapping users to videos for user.saved_videos
+#User.liked_videos, and user.watched_videos User properties
+class UserSavedVideo(models.Model):
+	user = models.ForeignKey('User')
+	video = models.ForeignKey('Video')
+	liked = models.BooleanField()
+	watched = models.BooleanField()
+
+
+class UserLikedVideo(models.Model):
+	user = models.ForeignKey('User')
+	video = models.ForeignKey('Video')
+	saved = models.BooleanField()
+	watched = models.BooleanField()
+
+class UserWatchedVideo(models.Model):
+	user = models.ForeignKey('User')
+	video = models.ForeignKey('Video')
