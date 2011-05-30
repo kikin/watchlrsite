75ff49f3ed75cea93277144744d64b266375213e
Nick Zaillian <naz2106@columbia.edu>
2011-05-30 17:49:08 -0400

    added a few more m2m convenience functions to User model.

--- static/js/kikin_video_panel.js
+++ static/js/kikin_video_panel.js
@@ -1,7 +1,25 @@
+com.kikin.VideoPanelController = function(){
+
+    return {
+
+        stylizeVideoTitles : function(){
+                Cufon.replace('h3.video-title, .section-title, h4', {
+                fontFamily: 'vag',
+                forceHitArea: true,
+                hover: true
+            });
+        },
+
+        populatePanel : function(panel_container_selector, contentSource, request_params){
+            $.get(contentSource, request_params, function(data){
+                $(panel_container_selector).empty();
+                $(panel_container_selector).html(data);
+                this.stylizeVideoTitles();
+            });
+        }
+    };
+
+};
 $(document).ready(function(){
-    Cufon.replace('h3.video-title, .section-title, h4', {
-        fontFamily: 'vag',
-        forceHitArea: true,
-        hover: true
-    });
+
 });
