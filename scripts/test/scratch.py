483ae90efdda92100b73d993f1b4e1948d55328f
Nick Zaillian <naz2106@columbia.edu>
2011-05-30 16:30:29 -0400

    implemented a convenient accessor for User model's 'liked_videos' m2m

---
+++ scripts/test/scratch.py
@@ -0,0 +1,21 @@
+import sys, os
+
+# the settings module is in the project root package 3 dirs up...
+# (ASSUMING you are execting this script from within the
+# directory containing it)
+
+sys.path.append(os.path.abspath('../../../'))
+
+#in case invoking from top-level project directory...
+sys.path.append(os.path.abspath('.'))
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'kikinvideo.settings'
+
+from kikinvideo.models import *
+
+u = User.objects.get(pk=2)
+'''
+v = Video.objects.all()[0]
+u.like_video(v)'''
+l = u.get_liked_videos()
+l
