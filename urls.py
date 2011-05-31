8c0acc1a810ae7bd224a0f9476c2dbbeca2bbe5f
Nick Zaillian <naz2106@columbia.edu>
2011-05-30 20:54:34 -0400

    cleanup

--- urls.py
+++ urls.py
@@ -3,20 +3,7 @@ from django.conf.urls.static import static
 import socialregistration
 import settings
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'video.views.home', name='home'),
-    # url(r'^video/', include('video.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
 	(r'^', include('kikinvideo.webapp.urls')),
     (r'^api/', include('kikinvideo.api.urls')),
     url(r'', include('social_auth.urls')),
