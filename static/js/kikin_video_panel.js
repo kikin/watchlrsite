455cc56b30c7b08b43a037e32eeca008ded5ca16
Nick Zaillian <naz2106@columbia.edu>
2011-05-31 10:43:47 -0400

    little frontend work: heart button + associated event handling

--- static/js/kikin_video_panel.js
+++ static/js/kikin_video_panel.js
@@ -8,12 +8,29 @@ com.kikin.VideoPanelController = function(){
                 });
     }
 
+    var LIKED_ICON_CONTAINER_SELECTOR = ".heart-container";
     return {
         populatePanel : function(panel_container_selector, contentSource, request_params){
             $.get(contentSource, request_params, function(data){
                 $(panel_container_selector).empty();
                 $(panel_container_selector).html(data);
                 _stylizeVideoTitles();
+                $(LIKED_ICON_CONTAINER_SELECTOR).each(function(){
+                    $(this).mouseover(function(){
+                        if($(this).hasClass('no-hover'))
+                            $(this).removeClass('no-hover');
+                        if(!$(this).hasClass('hovered'))
+                            $(this).addClass('hovered');
+                    });
+
+                    $(this).mouseout(function(){
+                        if($(this).hasClass('hovered'))
+                            $(this).removeClass('hovered');
+                        if(!$(this).hasClass('no-hover'))
+                            $(this).addClass('no-hover');
+                    });
+                });
+                
             });
         }
     };
