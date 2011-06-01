6cbdfecd48861b7691ecd2cced751ad902c3f04f
Sandesh Devaraju <sandesh@kikin.com>
2011-06-01 16:37:38 -0400

    rest of api methods

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,7 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns(
     'api',
     (r'^like/(?P<video_id>[0-9]+)$', 'views.like'),
+    (r'^unlike/(?P<video_id>[0-9]+)$', 'views.unlike'),
+    (r'^save/(?P<video_id>[0-9]+)$', 'views.save'),
+    (r'^remove/(?P<video_id>[0-9]+)$', 'views.remove'),
 )
