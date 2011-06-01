b6f24565431c7fb9aafeb16b3b1f0fc97e2429f7
Sandesh Devaraju <sandesh@kikin.com>
2011-06-01 19:15:10 -0400

    username now composed properly

--- api/models.py
+++ api/models.py
@@ -4,19 +4,53 @@ from django.contrib.auth import models as auth_models
 from datetime import datetime
 
 class Source(models.Model):
+    '''
+    Video source
+
+    YouTube is a an example
+    >>> youtube = Source.objects.create(name='YouTube', url='http://youtube.com',
+    ... favicon='http://youtube.com/favicon.ico')
+
+    Source `name` should be unique
+    >>> ripoff = Source.objects.create(name='YouTube', url='http://facetube.com') # doctest: +IGNORE_EXCEPTION_DETAIL
+    Traceback (most recent call last):
+      ...
+    IntegrityError: duplicate key value violates unique constraint "api_source_name_key"
+    '''
+
     name = models.CharField(max_length=100, unique=True)
     url = models.URLField(max_length=750, verify_exists=False)
-    favicon = models.URLField(max_length=750, verify_exists=False)
+    favicon = models.URLField(max_length=750, verify_exists=False, null=True)
 
 
 class Video(models.Model):
-    url = models.URLField(max_length=750, verify_exists=False)
-    title = models.CharField(max_length=500, db_index=True)
-    description = models.TextField(max_length=3000)
+    '''
+    Video object encapsulates metadata
+
+    Example YouTube video
+    >>> video = Video.objects.create(url='http://www.youtube.com/watch?v=kh29_SERH0Y')
+
+    Set some metadata
+    >>> video.title = 'The Art of FLIGHT'
+    >>> video.save()
+
+    Add source
+    >>> youtube = Source.objects.create(name='YouTube', url='http://youtube.com')
+    >>> video.source = youtube
+
+    Set thumbnail
+    >>> video.set_thumbnail('http://i2.ytimg.com/vi/kh29_SERH0Y/0.jpg', 480, 360)
+    >>> video.get_thumbnail().url
+    u'http://i2.ytimg.com/vi/kh29_SERH0Y/0.jpg'
+    '''
+
+    url = models.URLField(max_length=750, verify_exists=False, db_index=True)
+    title = models.CharField(max_length=500, db_index=True, null=True)
+    description = models.TextField(max_length=3000, null=True)
     html_embed_code = models.TextField(max_length=3000, null=True)
     html5_embed_code = models.TextField(max_length=3000, null=True)
     source = models.ForeignKey(Source, related_name='videos', null=True)
-    host = models.URLField(max_length=750, verify_exists=False)
+    host = models.URLField(max_length=750, verify_exists=False, null=True)
     fetched = models.DateTimeField(null=True, db_index=True)
 
     def set_thumbnail(self, url, width, height, type='web'):
@@ -27,44 +61,60 @@ class Video(models.Model):
 
 
 class Thumbnail(models.Model):
+    '''
+    Video thumbnails
+
+    Thumbnails are just an URL with size info
+    >>> video = Video.objects.create(url='http://www.youtube.com/watch?v=kh29_SERH0Y')
+    >>> thumb = Thumbnail.objects.create(video=video, url='http://i2.ytimg.com/vi/kh29_SERH0Y/0.jpg',
+    ... width=480, height=360)
+
+    Default to `web` type
+    >>> thumb.type
+    u'web'
+
+    A video can have multiple thumbnails
+    >>> mobile_thumb = Thumbnail.objects.create(video=video, url='http://i2.ytimg.com/vi/kh29_SERH0Y/2.jpg',
+    ... width=120, height=90, type='mobile')
+    >>> Video.objects.get(url=video.url).thumbnails.all()
+    [http://i2.ytimg.com/vi/kh29_SERH0Y/2.jpg, http://i2.ytimg.com/vi/kh29_SERH0Y/0.jpg]
+    '''
+
     video = models.ForeignKey(Video, related_name='thumbnails')
     type = models.CharField(max_length=10, default='web')
     url = models.URLField(max_length=750, verify_exists=False)
     width = models.IntegerField()
     height = models.IntegerField()
 
+    def __repr__(self):
+        return self.url
 
-#note: because we're now subclassing django.contrib.auth.User
-#for this model, we also get the email field
-#from our spec and an auto-incrementing id field.
-#There is a password field as well, but if no
-#password is provided when creating and
-#saving a User instance, it will simply be marked as having
-#an unusable password (see https://docs.djangoproject.com/en/dev
-#/topics/auth/#django.contrib.auth.models.User.set_unusable_password)
-#and we can let the facebook auth backend handle authentication.
+
 class User(auth_models.User):
+    '''
+    User object. Extends `django.contrib.auth.User`.
+    As a result, we get username, email, id and password (unused) fields.
+
+    Only required field is `username`
+    >>> user = User.objects.create(username='bender')
+
+    Which, of course, is required to be unique
+    >>> evil_twin = User.objects.create(username='bender') # doctest: +IGNORE_EXCEPTION_DETAIL
+    Traceback (most recent call last):
+      ...
+    IntegrityError: duplicate key value violates unique constraint "auth_user_username_key"
+    '''
     videos = models.ManyToManyField(Video, through='UserVideo')
     follows = models.ManyToManyField('self', symmetrical=False)
 
     # Use UserManager to get the create_user method, etc.
     objects = auth_models.UserManager()
 
-    #we initially had facebook_access_token and uid columns/properties
-    #for this model, but the user's token and uid are already being
-    #stored in the extra_data property of the UserSocialAuth model
-    #built into django-social-auth, so you can use the following instance
-    #methods to access these things...
     def facebook_access_token(self):
         return self.social_auth.get().extra_data['access_token']
 
     def facebook_uid(self):
         return self.social_auth.get().uid
-
-    #note:
-    #   For our own convenience, we should have
-    #   functions for working more seamlessly with the m2m
-    #   properties of this model:
 
     def _create_or_update_video(self, video, **kwargs):
         properties = ('liked', 'saved', 'watched')
@@ -138,3 +188,39 @@ class UserVideo(models.Model):
                  'likes': UserVideo.objects.filter(video=self.video, liked=True).count(),
                  'saved': self.saved,
                  'saves': UserVideo.objects.filter(video=self.video, saved=True).count() }
+
+
+import unicodedata, re
+from django.template.defaultfilters import stringfilter
+
+def slugify(username, id):
+    '''
+	    Normalizes string, converts to lowercase, removes non-alphanumeric characters (including spaces).
+	    Also, checks and appends offset integer to ensure that username is unique.
+    '''
+    username = unicodedata.normalize('NFKD', username).encode('ascii', 'ignore')
+    username = basename = unicode(re.sub('[^0-9a-zA-Z]+', '', username).strip().lower())
+    counter = 1
+    while True:
+        try:
+            found = User.objects.get(username=username)
+            if found.id == id:
+                return username
+        except User.DoesNotExist:
+            break
+        username = '%s%d' % (basename, counter)
+        counter += 1
+    return username
+slugify.is_safe = True
+slugify = stringfilter(slugify)
+
+from social_auth.signals import pre_update
+from social_auth.backends.facebook import FacebookBackend
+
+def facebook_extra_values(sender, user, response, details, **kwargs):
+    fullname = ''.join([user.first_name, user.last_name])
+    user.username = response.get('username', slugify(fullname, user.id))
+    return True
+
+pre_update.connect(facebook_extra_values, sender=FacebookBackend)
+
