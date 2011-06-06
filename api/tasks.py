351d96030147e6b73ba3a7f5832b8431d3914a97
Sandesh Devaraju <scouredimage@gmail.com>
2011-06-06 19:09:52 -0400

    escape %

--- api/tasks.py
+++ api/tasks.py
@@ -582,7 +582,7 @@ class YoutubeFetcher(object):
   </embed>
 </object>'''
 
-    YOUTUBE_HTML5_EMBED_TAG = '''<iframe type="text/html" width="100%" height="100%"
+    YOUTUBE_HTML5_EMBED_TAG = '''<iframe type="text/html" width="100%%" height="100%%"
     src="http://www.youtube.com/embed/%(id)s?enablejsapi=1&autoplay=1" frameborder="0">'''
 
     def __init__(self):
@@ -816,7 +816,7 @@ class VimeoFetcher(object):
          type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="640" height="360"></embed>
 </object>'''
 
-    VIMEO_HTML5_EMBED_TAG = '''<video src="http://www.vimeo.com/play_redirect?clip_id=%(id)s" controls="controls" width="100%" height="100%"></video>'''
+    VIMEO_HTML5_EMBED_TAG = '''<video src="http://www.vimeo.com/play_redirect?clip_id=%(id)s" controls="controls" width="100%%" height="100%%"></video>'''
 
     def sources(self):
         return (self.SOURCE,)
