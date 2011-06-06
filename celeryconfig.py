f3d4c63fe7979306153b790120ef3b5b0b3e22ac
Sandesh Devaraju <scouredimage@gmail.com>
2011-06-06 15:14:16 -0400

    celery config

---
+++ celeryconfig.py
@@ -0,0 +1,22 @@
+# List of modules to import when celery starts.
+CELERY_IMPORTS = ("api.tasks", )
+
+# broker settings
+BROKER_HOST = "localhost"
+BROKER_PORT = 5672
+BROKER_USER = "guest"
+BROKER_PASSWORD = "guest"
+BROKER_VHOST = "/"
+
+# No tasks currently produce results or use rate limits
+CELERY_IGNORE_RESULT = True
+CELERY_DISABLE_RATE_LIMITS = True
+
+CELERY_ALWAYS_EAGER = True
+TEST_RUNNER = 'djcelery.contrib.test_runner.run_tests'
+
+CELERYD_CONCURRENCY = 10
+
+CELERYD_LOG_FILE = "celeryd.log"
+CELERYD_LOG_LEVEL = "DEBUG"
+  
