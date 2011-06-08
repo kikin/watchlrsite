aa226be8fa798b04180e99ccfb31fd3dbd17d835
Nick Zaillian <naz2106@columbia.edu>
2011-06-08 16:06:47 -0400

    html5 playback working with default players for each browser

--- static/js/kikin_user_home.js
+++ static/js/kikin_user_home.js
@@ -176,5 +176,7 @@ $(document).ready(
 
             //in case we're refreshing...
             homeViewController.onHashChange(location.hash);
+
+            VideoJS.setupAllWhenReady();
         }
 );
