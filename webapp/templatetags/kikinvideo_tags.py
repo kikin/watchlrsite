64fd018090b5580ccd4c93dcedf4c7dccaf78005
Nick Zaillian <naz2106@columbia.edu>
2011-06-15 18:54:20 -0400

    hotfix

--- webapp/templatetags/kikinvideo_tags.py
+++ webapp/templatetags/kikinvideo_tags.py
@@ -178,7 +178,7 @@ def activity_item_heading(activity_item, user):
             content += '<a href="/'+activity_item.users[0][0].username+'">'+activity_item.users[0][0].first_name+'</a> liked...'
         elif len(activity_item.users) == 1 and len(all_likers) == 2:
             content += '<a href="/'+activity_item.users[0][0].username+'">'+activity_item.users[0][0].first_name+'</a>'
-            content += 'and 1 other liked...'
+            content += ' and 1 other liked...'
         elif len(activity_item.users) == 1 and len(all_likers) > 2:
             content += '<a href="/'+activity_item.users[0][0].username+'">'+activity_item.users[0][0].first_name+'</a>'
             content += ' and ' + str(len(all_likers) - 1) + ' others liked...'
