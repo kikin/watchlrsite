e3ef99323a7af999be255dd4c4c20f2a74422abd
Nick Zaillian <naz2106@columbia.edu>
2011-06-16 17:17:08 -0400

    "follow/unfollow" fixes

--- api/models.py
+++ api/models.py
@@ -213,7 +213,7 @@ class User(auth_models.User):
         return user_video
 
     def followers(self):
-        return [u.follower for u in self.follower_set]
+        return [u.follower for u in self.follower_set.all()]
 
     def following(self):
         return list(self.follows.all())
