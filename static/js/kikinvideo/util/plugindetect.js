29f896ba357f483f3c9e321fedbb5f806bb6e6a2
Nick Zaillian <naz2106@columbia.edu>
2011-06-16 16:13:33 -0400

    plugin pitch will now display in unsupported browsers (previously only displayed in supported browsers)

--- static/js/kikinvideo/util/plugindetect.js
+++ static/js/kikinvideo/util/plugindetect.js
@@ -1,21 +1,16 @@
-if(kikinvideo.util.Kikin.isSupportedClientAgent()){
-    var iChromePluginInitTries = 0;
-    $(window).load(function() {
+var iChromePluginInitTries = 0;
+$(window).load(function() {
 
-        if(iChromePluginInitTries<5 && kikinvideo.util.Kikin.isChromeInstallation() && !window.kikin){
-            iChromePluginInitTries++
-            setTimeout(arguments.callee, 500);
-            return;
-        }
+    if(iChromePluginInitTries<5 && kikinvideo.util.Kikin.isChromeInstallation() && !window.kikin){
+        iChromePluginInitTries++
+        setTimeout(arguments.callee, 500);
+        return;
+    }
 
-        if(!kikinvideo.util.Kikin.createPlugin()){
-            $.get('/content/plugin_pitch', function(content){
-                    $('.right-panel').prepend(content);
-                    $('#plugin-pitch').fadeIn(1000);
-               //     $('#plugin-pitch-close').click(function(){
-               //             $('#plugin-pitch').fadeOut();
-               //     });
-            });
-        }
-    });
-}
+    if(!kikinvideo.util.Kikin.createPlugin()){
+        $.get('/content/plugin_pitch', function(content){
+                $('.right-panel').prepend(content);
+                $('#plugin-pitch').fadeIn(1000);
+        });
+    }
+});
