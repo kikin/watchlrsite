788aa01fe4fadffce01c37ab4d1d648e18d1eaf3
Nicholas Zaillian <naz2106@columbia.edu>
2011-07-13 16:30:53 -0400

    just check the diffs

--- static/js/kikinvideo/downloadpitch.js
+++ static/js/kikinvideo/downloadpitch.js
@@ -1,15 +1,11 @@
 var kkn = kikinvideo.util.Kikin;
 
 function browserSpecificPitch(){
-    if(kkn.isMac() && $.browser.chrome){
+    if($.browser.chrome){
         $('#downloadButtonContainer').html('<a href="http://www.watchlr.com/static/downloads/watchlr_installer_1.0.1.crx"><div class="downloadapp">&nbsp;</div></a>')
     }
     else if($.browser.mozilla){
-        if(kkn.isMac()){
-            $('#downloadButtonContainer').html('<a href="http://www.watchlr.com/static/downloads/watchlr_installer_1.0.1.xpi"><div class="downloadapp">&nbsp;</div></a>');
-        }else if(kkn.isWindows()){
-            $('#downloadButtonContainer').html('<a href="http://www.watchlr.com/static/downloads/watchlr_installer_1.0.1.xpi"><div class="downloadapp">&nbsp;</div></a>')
-        }
+        $('#downloadButtonContainer').html('<a href="http://www.watchlr.com/static/downloads/watchlr_installer_1.0.1.xpi"><div class="downloadapp">&nbsp;</div></a>')
     }else{
         $('#downloadButtonContainer').html('Your browser is not supported (we support Chrome and Firefox on Mac and PC).');
     }
