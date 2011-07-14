74f2628871347503e36981d449e0fe70ddbeb190
Nicholas Zaillian <naz2106@columbia.edu>
2011-07-14 11:06:20 -0400

    plugin link href fixed

--- static/js/kikinvideo/downloadpitch.js
+++ static/js/kikinvideo/downloadpitch.js
@@ -2,10 +2,10 @@ var kkn = kikinvideo.util.Kikin;
 
 function browserSpecificPitch(){
     if($.browser.chrome){
-        $('#downloadButtonContainer').html('<a href="http://www.watchlr.com/static/downloads/watchlr_installer_1.0.1.crx"><div class="downloadapp">&nbsp;</div></a>')
+        $('#downloadButtonContainer').html('<a href="http://download.watchlr.com/static/plugin/WatchlrInstaller.crx"><div class="downloadapp">&nbsp;</div></a>')
     }
     else if($.browser.mozilla){
-        $('#downloadButtonContainer').html('<a href="http://www.watchlr.com/static/downloads/watchlr_installer_1.0.1.xpi"><div class="downloadapp">&nbsp;</div></a>')
+        $('#downloadButtonContainer').html('<a href="http://download.watchlr.com/static/plugin/WatchlrInstaller.xpi"><div class="downloadapp">&nbsp;</div></a>')
     }else{
         $('#downloadButtonContainer').html('Your browser is not supported (we support Chrome and Firefox on Mac and PC).');
     }
