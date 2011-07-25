dafdb1531ac44f079e08b811b2cd0d62dafde3da
Sandesh Devaraju <scouredimage@gmail.com>
2011-07-25 18:35:59 -0400

    now tracked in watchlr-download repo

--- plugin/Chrome/kikin_domain.js
+++
@@ -1,7 +0,0 @@
-// Inject the kikin object so that we 
-// can identify if plugin is installed
-// or not
-var div = document.createElement('div');
-div.id = 'kikin_dummy_element_for_plugin_detection';
-div.style.display = 'hidden';
-document.body.appendChild(div);
