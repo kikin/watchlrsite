612824d1dabc3716808a03e91064418192eff235
Sandesh Devaraju <scouredimage@gmail.com>
2011-07-28 12:08:22 -0400

    tracking changes

--- analytics/models.py
+++ analytics/models.py
@@ -3,7 +3,7 @@ from django.db import models
 UNAUTHORIZED_USER = 'ANONYMOUS'
 
 class Base(models.Model):
-    agent = models.CharField(max_length=10, null=True, db_index=True)
+    agent = models.CharField(max_length=20, null=True, db_index=True)
     agent_version = models.CharField(max_length=10, null=True, db_index=True)
     context = models.URLField(verify_exists=False, null=True)
     country = models.CharField(max_length=10, null=True, db_index=True)
@@ -17,7 +17,7 @@ class Base(models.Model):
 class Activity(Base):
     user_id = models.CharField(max_length=20, db_index=True)
     action = models.CharField(max_length=50, db_index=True)
-    secondary_id = models.PositiveIntegerField(null=True, db_index=True)
+    secondary_id = models.CharField(max_length=200, null=True, db_index=True)
 
 
 class Event(Base):
