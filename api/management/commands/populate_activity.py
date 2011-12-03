4134449f1f843356fb7c40c01c76ae10e86ea551
Sandesh Devaraju <scouredimage@gmail.com>
2011-12-03 12:16:12 -0500

    skip over entries with videos now deleted?

--- api/management/commands/populate_activity.py
+++ api/management/commands/populate_activity.py
@@ -3,7 +3,7 @@ from operator import attrgetter
 
 from django.core.management.base import BaseCommand
 
-from api.models import UserVideo, Activity
+from api.models import UserVideo, Video, Activity
 
 
 class Command(BaseCommand):
@@ -19,12 +19,16 @@ class Command(BaseCommand):
                 users = filter(attrgetter('is_registered'), uservideo.user.facebook_friends())
 
             for user in users:
-                activity, created = Activity.objects.get_or_create(user=user,
-                                                                   friend=uservideo.user,
-                                                                   video=uservideo.video,
-                                                                   action=action)
+                try:
+                    activity, created = Activity.objects.get_or_create(user=user,
+                                                                       friend=uservideo.user,
+                                                                       video=uservideo.video,
+                                                                       action=action)
 
-                if created:
-                    timestamp = uservideo.liked_timestamp if action == 'like' else uservideo.shared_timestamp
-                    activity.expire_time = timestamp + timedelta(days=14)
-                    activity.save()
+                    if created:
+                        timestamp = uservideo.liked_timestamp if action == 'like' else uservideo.shared_timestamp
+                        activity.expire_time = timestamp + timedelta(days=14)
+                        activity.save()
+
+                except Video.DoesNotExist:
+                    pass
